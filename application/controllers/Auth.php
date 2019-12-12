edbfb62c9f2004d22c0d971476d0bbb4175c3429
atulpasare1 <atulpasare1@gmail.com>
2019-12-12 13:25:29 +0530

    Update Auth.php
    test Change

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -12,10 +12,7 @@ class Auth extends Admin_Controller
 		$this->load->model('model_auth');
 	}
 
-	/* 
-		Check if the login form is submitted, and validates the user credential
-		If not submitted it redirects to the login page
-	*/
+
 	public function login()
 	{
 
